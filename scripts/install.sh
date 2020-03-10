8aa9c5af33911de689e693a443993e3edd5b8778
Ramiz Polić <ramiz.polic@hotmail.com>
2020-03-10 21:08:14 +0100

    adding permissions

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,12 +10,12 @@ sudo apt-get upgrade
 
 # Install required packages
 # -------------------------
-sudo apt-get install build-essential libtool autotools-dev autoconf pkg-config libssl-dev
-sudo apt-get install libboost-all-dev
-sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
-sudo apt-get install libqrencode-dev autoconf openssl libssl-dev libevent-dev
-sudo apt-get install libminiupnpc-dev
-sudo apt-get install libleveldb-dev
+sudo apt-get install -y build-essential libtool autotools-dev autoconf pkg-config libssl-dev
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+sudo apt-get install -y libqrencode-dev autoconf openssl libssl-dev libevent-dev
+sudo apt-get install -y libminiupnpc-dev
+sudo apt-get install -y libleveldb-dev
 
 # Install Berkley DB 4.8
 # ----------------------

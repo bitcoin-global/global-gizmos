4a3d2a23911cbc3717c8b631de1642c4d7ee6272
Ramiz Polić <ramiz.polic@hotmail.com>
2020-06-19 04:53:07 +0200

    src: update builder

---
+++ builder/build.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+set -xeo pipefail
+
+
+GD_ENV_FILE=${1:-maza.env}
+export GD_OS_PACKAGE=${2}
+
+case ${GD_OS_PACKAGE} in 
+linux)
+      echo "Building for Linux"
+      ;;
+  win)
+      echo "Building for Windows "
+      ;;
+  osx)
+      echo "Building for OSX"
+      ;;
+  all)
+      echo "Building for Windows Linux and OSX"
+      export GD_OS_PACKAGE="osx linux win"
+      ;;
+    *)
+      echo "Usage: ./build.sh [env-file] [win|linux|osx|all]"
+      exit 1
+      ;;
+esac
+
+test -z ${TRAVIS_BUILD_DIR} || export GD_BUILDER=TRAVIS
+if [ "${GD_BUILDER}" = "TRAVIS" ]; then 
+   echo "GD_BUILDER=TRAVIS" >> ${GD_ENV_FILE} 
+fi
+export $(cat ${GD_ENV_FILE}  |egrep -v '^#' | xargs)
+
+# get the local UID, and make sure we build the containers 
+# with gitian user mapped to this
+# ensures write access to output directories
+LOCAL_USER=$(whoami)
+# TBH I don't know how portable this is, 
+# so, we'll bail if we don't have UID
+ME=${UID}
+
+function die {
+  echo "you do not have a UID=\"${UID}\" set in your environment"
+  exit 1
+}
+test -z ${ME} && die 
+if [ "${ME}" = "0" ] ; then
+  echo  "Seriously. let's make this run without root privs"
+   exit 2
+fi
+test -f ${GD_HOST_OSXSDK} || exit 3
+
+NAMESPACE=${LOCAL_USER:-gitianbuild}
+
+echo "rm -rf ${NAMESPACE} Stage1 Dockerfile.stage2 Stage2 Dockerfile clean.sh" > ./clean.sh
+chmod +x ./clean.sh 
+
+# make a build dir for Docker 
+# this prevents Docker context from including 20GB of 
+# base-vm files in subsequent runs 
+rm -rf Stage1
+mkdir Stage1 
+cp ${GD_HOST_OSXSDK} Stage1
+cp Dockerfile.stage1 Stage1
+cp gitian_build.sh Stage1
+cp make_gitian_vms.sh Stage1
+cp config-lxc Stage1
+cp ${GD_ENV_FILE} Stage1
+cp sources.list-wily Stage1
+cd Stage1
+sed 's/LOCAL_UID/'${ME}'/g' Dockerfile.stage1 > Dockerfile
+docker build -f Dockerfile -t ${NAMESPACE}/gitian-stage1 . 
+cd ..
+
+mkdir -pv $(pwd)/${NAMESPACE}/gitian-builder/inputs
+cp ${GD_HOST_OSXSDK} $(pwd)/${NAMESPACE}/gitian-builder/inputs
+mkdir -pv $(pwd)/${NAMESPACE}/gitian-builder/var
+mkdir -pv $(pwd)/${NAMESPACE}/${GD_BUILD_COIN}-src
+sudo chown -R  ${ME}  $(pwd)/${NAMESPACE}
+
+#docker run -it --rm --privileged --volumes-from gitian_data  ${NAMESPACE}/gitian-stage1
+docker run -it --rm --privileged -v $(pwd)/${NAMESPACE}:/data ${NAMESPACE}/gitian-stage1
+
+rm -rf Stage2
+mkdir Stage2
+
+echo -e "FROM ${NAMESPACE}/gitian-stage1" > Dockerfile.stage2
+echo -e "ENTRYPOINT [\"/gitian/gitian_build.sh\"]" >> Dockerfile.stage2
+
+cp Dockerfile.stage2 Stage2
+cd Stage2
+
+docker build -f Dockerfile.stage2 -t ${NAMESPACE}/gitian-builder . 
+cd ..
+cp ${GD_ENV_FILE} $(pwd)/${NAMESPACE}/gitian-builder
+
+for os_pkg in ${GD_OS_PACKAGE} ; do
+    docker run -it --rm  --privileged --env-file ${GD_ENV_FILE}  -v $(pwd)/${NAMESPACE}/gitian-builder:/gitian/gitian-builder -v $(pwd)/${NAMESPACE}/${GD_BUILD_COIN}:/gitian/${GD_BUILD_COIN}  ${NAMESPACE}/gitian-builder ${GD_OS_PACKAGE}
+done
+
